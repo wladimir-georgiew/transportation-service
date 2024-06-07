97a7219e3d96a6b1f18ed26aa7b1dbd813e4abdf
Vladimir Georgiev <vladimir.georgiev.dev@gmail.com>
2024-06-07 06:12:49 +0300

    fix

--- src/Components/TopBar.js
+++ src/Components/TopBar.js
@@ -6,23 +6,23 @@ function App() {
           <div className="col-12">
             <a href="#" className="">
               <span className="mr-2  icon-envelope-open-o"></span>{" "}
-              <span className="d-none d-md-inline-block">info@yourdomain.com</span>
+              <span className="d-md-inline-block">info@yourdomain.com</span>
             </a>
             <span className="mx-md-2 d-inline-block"></span>
             <a href="#" className="">
               <span className="mr-2  icon-phone"></span>{" "}
-              <span className="d-none d-md-inline-block">1+ (234) 5678 9101</span>
+              <span className="d-md-inline-block">1+ (234) 5678 9101</span>
             </a>
 
             <div className="float-right">
               <a href="#" className="">
                 <span className="mr-2  icon-twitter"></span>{" "}
-                <span className="d-none d-md-inline-block">Twitter</span>
+                <span className="d-md-inline-block">Twitter</span>
               </a>
               <span className="mx-md-2 d-inline-block"></span>
               <a href="#" className="">
                 <span className="mr-2  icon-facebook"></span>{" "}
-                <span className="d-none d-md-inline-block">Facebook</span>
+                <span className="d-md-inline-block">Facebook</span>
               </a>
             </div>
           </div>
