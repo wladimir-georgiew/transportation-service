44d157d42283095d076dfc2e51995bf239fd6f39
Vladimir Georgiev <vladimir.georgiev.dev@gmail.com>
2024-06-11 03:35:04 +0300

    add faq translations  template

--- src/Components/Sections/FAQ.js
+++ src/Components/Sections/FAQ.js
@@ -1,15 +1,18 @@
 import Accordion from "@mui/material/Accordion";
 import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { useTranslation } from "react-i18next";
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <div className="site-section bg-light" id="faq-section">
       <div className="container">
         <div className="row mb-5">
           <div className="block-heading-1 col-12 text-center">
-            <h2>Frequently Ask Questions</h2>
+            <h2>{t("faq.faq_name")}</h2>
           </div>
         </div>
         <div className="row">
@@ -20,13 +23,9 @@ function App() {
                 aria-controls="panel1-content"
                 id="panel1-header"
               >
-                Accordion 1
+                {t("faq.q1")}
               </AccordionSummary>
-              <AccordionDetails>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </AccordionDetails>
+              <AccordionDetails>{t("faq.q1_details")}</AccordionDetails>
             </Accordion>
             <Accordion>
               <AccordionSummary
@@ -34,13 +33,9 @@ function App() {
                 aria-controls="panel1-content"
                 id="panel1-header"
               >
-                Accordion 1
+                {t("faq.q2")}
               </AccordionSummary>
-              <AccordionDetails>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </AccordionDetails>
+              <AccordionDetails>{t("faq.q2_details")}</AccordionDetails>
             </Accordion>
 
             <Accordion>
@@ -49,13 +44,9 @@ function App() {
                 aria-controls="panel1-content"
                 id="panel1-header"
               >
-                Accordion 1
+                {t("faq.q3")}
               </AccordionSummary>
-              <AccordionDetails>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </AccordionDetails>
+              <AccordionDetails>{t("faq.q3_details")}</AccordionDetails>
             </Accordion>
 
             <Accordion>
@@ -64,13 +55,9 @@ function App() {
                 aria-controls="panel1-content"
                 id="panel1-header"
               >
-                Accordion 1
+                {t("faq.q4")}
               </AccordionSummary>
-              <AccordionDetails>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </AccordionDetails>
+              <AccordionDetails>{t("faq.q4_details")}</AccordionDetails>
             </Accordion>
 
             <Accordion>
@@ -79,13 +66,9 @@ function App() {
                 aria-controls="panel1-content"
                 id="panel1-header"
               >
-                Accordion 1
+                {t("faq.q5")}
               </AccordionSummary>
-              <AccordionDetails>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                Suspendisse malesuada lacus ex, sit amet blandit leo lobortis
-                eget.
-              </AccordionDetails>
+              <AccordionDetails>{t("faq.q5_details")}</AccordionDetails>
             </Accordion>
           </div>
         </div>
